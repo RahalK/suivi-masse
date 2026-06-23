fbb9c893e6d29eb5e3dacf3203f418ad38674b64
Karen RAHAL <karen.rahal2@gmail.com>
2026-06-23 13:46:20 +0200

    Reorganize project structure

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-import pandas as pd
 import streamlit as st
 from datetime import date
-from sheets import ajouter_masse, recuperer_donnees
-from analytics import preparer_donnees
+import matplotlib.pyplot as plt
 
-data = recuperer_donnees()
-df = preparer_donnees(data)
+from src.sheets import ajouter_masse, recuperer_donnees
+from src.analytics import preparer_donnees
 
-st.title("Suivi de la masse (en kg) de Karen")
 
-st.subheader("Évolution de la masse")
+def main():
 
-if not df.empty:
-    st.line_chart(df.set_index("date")[["masse", "moyenne_7j"]])
-else:
-    st.info("Aucune donnée pour le moment")
+    data = recuperer_donnees()
+    df = preparer_donnees(data)
 
-masse = st.number_input("Masse (kg)", min_value=0.0, step=0.1)
+    st.title("Suivi de la masse (en kg) de Karen")
 
-if st.button("Ajouter") and masse > 0:
-    ajouter_masse(str(date.today()), masse)
-    st.success("Ajouté dans Google Sheets ✅")
+    if not df.empty:
+
+        fig, ax = plt.subplots()
+
+        ax.plot(df["date"], df["masse"], marker="o", label="Masse")
+        ax.plot(df["date"], df["moyenne_7j"], label="Moyenne mobile (7 jours)")
+
+        ax.set_title("Évolution de la masse")
+        # ax.set_xlabel("Date")
+        ax.set_ylabel("Masse (kg)")
+        ax.set_ylim(55, 58)
+        plt.xticks(rotation=15)
+
+        ax.legend()
+
+        st.pyplot(fig)
+
+    else:
+        st.info("Aucune donnée pour le moment")
+
+    masse = st.number_input("Masse (kg)", min_value=0.0, step=0.1)
+
+    if st.button("Ajouter") and masse > 0:
+        ajouter_masse(str(date.today()), masse)
+        st.success("Ajouté dans Google Sheets ✅")
+
+
+if __name__ == "__main__":
+    main()
